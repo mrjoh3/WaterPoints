4f2882adce906036f1daec6207035bee7b855cf3
Matt Johnson <mrjoh3@gmail.com>
2025-05-05 15:19:50 +1000

    fix geojson link

--- process_gis.R
+++ process_gis.R
@@ -10,4 +10,4 @@ crs4326 <- shp |>
   select(id, address, location, maxvolume, strategic, sub_type) |>
   sf::st_transform("EPSG:4326")
 
-sf::write_sf(crs4326, "gis/waterpoints2.geojson")
+sf::write_sf(crs4326, "gis/waterpoints.geojson")
