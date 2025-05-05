1cd3907129d9be6a86a2151b6cfdb8aedfd5dba5
Matt Johnson <mrjoh3@gmail.com>
2025-05-05 18:01:41 +1000

    clean process and add layer type field

--- process_gis.R
+++ process_gis.R
@@ -7,7 +7,15 @@ library(dplyr)
 shp <- sf::read_sf("gis/FireWater/FireWater.shp") 
 
 crs4326 <- shp |>
-  select(id, address, location, maxvolume, strategic, sub_type) |>
+  mutate(group_type = case_when(
+    grepl("Tank", sub_type) ~ "Tank",
+    grepl("Hydrant", sub_type) ~ "Hydrant",
+    sub_type == "Dam" ~ "Dam",
+    TRUE ~ "Other"
+  )) |>
+  select(id, group_type, location, maxvolume, assett_own, sub_type) |>
   sf::st_transform("EPSG:4326")
 
-sf::write_sf(crs4326, "gis/waterpoints.geojson")
+# delete old layer first
+file.remove("gis/waterpoints.geojson")
+sf::write_sf(crs4326, "gis/waterpoints3.geojson")
